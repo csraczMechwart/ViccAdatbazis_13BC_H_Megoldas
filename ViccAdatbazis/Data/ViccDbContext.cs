c946a31f5e19242a8a75329cff2b6da06d7fca6e
Rácz Csaba <csracz.hu@gmail.com>
2024-10-08 17:51:38 +0200

    Add project files.

---
+++ ViccAdatbazis/Data/ViccDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ViccAdatbazis.Models;
+
+namespace ViccAdatbazis.Data
+{
+    public class ViccDbContext : DbContext
+    {
+        public ViccDbContext(DbContextOptions<ViccDbContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<Vicc> Viccek {  get; set; }
+    }
+}
---
+++ ViccAdatbazis/Models/Vicc.cs
@@ -0,0 +1,12 @@
+﻿namespace ViccAdatbazis.Models
+{
+    public class Vicc
+    {
+        public int Id { get; set; }
+        public string Tartalom { get; set; }
+        public string Feltolto { get; set; }
+        public int Tetszik { get; set; }
+        public int NemTetszik { get; set; }
+        public bool Aktiv { get; set; }
+    }
+}
